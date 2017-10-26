06ce353055d514dd640de9ef79a2804bafa0229c
wayne zeng <zengqw62@163.com>
2017-10-26 16:55:17 +0800

    setup karma env; inital test using mocha + char

---
+++ README.md
@@ -0,0 +1,9 @@
+# Practice ES6 ES7
+
+This repo is to practice javascript using syntax of ES6 and ES7.
+
+Mocha + Chai is used as testing framework to write test cases.
+
+Karma is used to auto-run the *.spec.js test files.
+
+Webpack is used to build *.js with babel env preset. Chrome is the target browser.
---
+++ test/function/index.spec.js
@@ -0,0 +1,13 @@
+import { firstname, location } from '../../src/function'
+
+const expect = chai.expect
+
+describe( ' genereate namd and location ', () => {
+    it('should generate firstname: tom', () => {
+        expect(firstname === 'tom').to.be.true
+    })
+
+    it('should generate location: Uta', () => {
+        expect(location === 'Uta').to.be.true
+    })
+})
